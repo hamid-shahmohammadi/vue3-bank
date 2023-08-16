f0e3363b3c64b91b69c154d756e918ad0656f698
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-08-16 14:02:03 +0330

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import myjson from '../data.json';
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1>حساب بانکی</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>عنوان حساب</th>
+            <th>شماره حساب</th>
+            <th>شماره شبا</th>
+            <th>وضعیت درگاه بانک</th>
+            <th>وضعیت کارت خوان</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="x in myjson">
+            <th>{{ x.account_title }}</th>
+            <th>{{ x.account_number }}</th>
+            <th>{{ x.shaba_number }}</th>
+            <th>{{ x.port_status }}</th>
+            <th>{{ x.card_reader_status }}</th>        
+          </tr>         
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
+<style scoped>
+table {
+        border-collapse: collapse;
+        border-radius: 30px;
+        border-style: hidden; /* hide standard table (collapsed) border */
+        box-shadow: 0 0 0 1px #666; /* this draws the table border  */ 
+    }
+
+    td {
+        border: 1px solid #ccc;
+    }
+</style>
